13c0244ea68dcdf1aa660a77c8faaf27f33af269
SveTu1704 <sv.hata@mail.ru>
2024-07-12 19:18:05 +0200

    Add files via upload

---
+++ homework№4.py
@@ -0,0 +1,8 @@
+my_string=input("Введите Вашу Фамилию" )
+str="Введите Вашу Фамилию"
+print(int(len(str)))
+print("my_string".upper())
+print("my_string".lower())
+print("my string".replace(" ",""))
+print(str[0])
+print(str[-1])
