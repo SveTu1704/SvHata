39e21394bf9489741de5861b848638ba1d3c95f4
SveTu1704 <sv.hata@mail.ru>
2024-07-17 20:07:56 +0200

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,16 +1,13 @@
-"1st ptogram"
+#"1st ptogram"
 print(9**0.5*5)
-"2nd ptogram"
+#"2nd ptogram"
 print(9.99>9.98 and 1000!=1000.1)
 print(9.99>9.98 or 1000!=1000.1)
-"3rd ptogram"
-print(1234%1000//10)
-print(5678//10%100)
-print(int(13.42))
-print(int(42.13))
-print(13.42%13*100)
-print(round(41.999999999999993))
-print(42.13%42*100)
-print(round(13.000000000000256))
-print(13==13 and 42==42)
+#"3rd ptogram"
+print(1234%1000//10+5678//10%100)
+#"4th program"
+print(int(13.42)==(int(42.13%42*100)))
+print(int(13.42%13*102)==(int(42.13)))
+print(int(13.42)==(int(42.13%42*100))) and (int(13.42%13*102)==(int(42.13)))
+
 
