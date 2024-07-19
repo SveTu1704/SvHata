9a87b0fb8c8d8016375930b1d8078da6ae6b1872
SveTu1704 <sv.hata@mail.ru>
2024-07-19 21:34:09 +0200

    Add files via upload

--- task1.py
+++ task1.py
@@ -6,8 +6,11 @@ print(9.99>9.98 or 1000!=1000.1)
 #"3rd ptogram"
 print(1234%1000//10+5678//10%100)
 #"4th program"
-print(int(13.42)==(int(42.13%42*100)))
-print(int(13.42%13*102)==(int(42.13)))
-print(int(13.42)==(int(42.13%42*100))) and (int(13.42%13*102)==(int(42.13)))
+a=13.42
+b=42.13
+ai,bi=int(a),int(b)
+af,bf=int((a-ai)*100),int((b-bi)*100)
+print(ai==bf or bi==af)
+
 
 
