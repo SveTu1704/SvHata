89423a46da1183e3ee23da21876c96a502035a4b
SveTu1704 <sv.hata@mail.ru>
2024-07-22 14:23:54 +0200

    Add files via upload

---
+++ if,elif,else.py
@@ -0,0 +1,13 @@
+first=158
+second=175
+third=186
+if first==second and second==third:
+    print(3)
+elif first==second:
+    print(2)
+elif second==third:
+    print(2)
+elif first==third:
+    print(2)
+else:
+    print(0)
