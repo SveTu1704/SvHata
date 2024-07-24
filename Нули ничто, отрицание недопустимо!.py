d55cc2575037084314c8d35510d0decad789baf8
SveTu1704 <sv.hata@mail.ru>
2024-07-24 13:18:43 +0200

    Add files via upload

---
+++ Нули ничто, отрицание недопустимо!.py
@@ -0,0 +1,6 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while len(my_list) != 0 and a < len(my_list)  and my_list[a]>=0 :
+    if my_list[a] != 0:
+        print(my_list[a])
+    a = a + 1
