06ad92f44b2cb8f966144d675234f70c8e8733d8
SveTu1704 <sv.hata@mail.ru>
2024-08-08 23:15:51 +0200

    Add files via upload

--- Модуль 2.5.py
+++ Модуль 2.5.py
@@ -1,16 +1,16 @@
-def get_matrix(n,m,value):
+def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
-        row = [value]*m
+        row = []
+        for j in range(m):
+            row.append(value)
         matrix.append(row)
         return matrix
-result1 = get_matrix(3,4,10)
-result2 = get_matrix(5,5,62)
-result3 = get_matrix(3,6,85)
+
+
+result1 = get_matrix(3, 4, 10)
+result2 = get_matrix(5, 5, 62)
+result3 = get_matrix(3, 6, 85)
 print(result1)
 print(result2)
 print(result3)
-
-
-
-
